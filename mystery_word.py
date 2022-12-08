4f421239f16fe9e943d4e320f8214174defd5ccf
jeromecox <jeromecox@hotmail.com>
2022-12-08 08:42:35 -0600

    still working on it

--- mystery_word.py
+++ mystery_word.py
@@ -9,34 +9,65 @@ def play_game():
     word_list = read_file.split()
     low_mystery_word = (random.choice(word_list))
     mystery_word = low_mystery_word.upper()
+    word_length = len(mystery_word)
+    unpacked = [*mystery_word]
+    blank_list = unpacked[:]
+
+    i = 0
+
+    while i < len(blank_list):
+        blank_list[i] = '_'
+        i += 1
+
+    print(*blank_list)
+
     print(mystery_word)
 
-    print(f'The mystery word is {len(mystery_word)} letters long.')
+    print(unpacked)
 
+    print(f'The mystery word is {word_length} letters long.')
+    print('')
     guess_count = 0
     guessed_list = []
-    while guess_count < 8:
+    while guess_count < 8 and '_' in blank_list:
+        print('')
+        print(f'Guesses remaining: {8 - guess_count}')
+        print('')
         print('Available letters to guess:')
         print(available_letters)
+        print('')
         guess = input('Guess a letter: ').upper()
         if not guess.isalpha() or len(guess) != 1:
+            print('')
             print('Invalid guess. Please guess one letter.')
             continue
         elif guess in guessed_list:
+            print('')
             print('You have already guessed this letter.')
             continue
         elif guess in mystery_word:
-            guess_count += 1
             available_letters.remove(guess)
+            guessed_list.append(guess)
+            
+            print('')
             print('Good guess!')
-            print(f'Guesses remaining: {8 - guess_count}')
+            print('')
+
             continue
         else:
             guess_count += 1
             available_letters.remove(guess)
+            guessed_list.append(guess)
+            print('')
             print('Incorrect guess')
-            print(f'Guesses remaining: {8 - guess_count}')
             continue
+
+    if '_' in blank_list:
+        print('')
+        print('Sorry! You lose!')
+    else:
+        print('')
+        print('Congratulations! You win!')
 
 
 if __name__ == "__main__":
