919a1e314de6041650ef67e23c4072ed98f91903
jeromecox <jeromecox@hotmail.com>
2022-12-08 10:41:29 -0600

    game works

--- mystery_word.py
+++ mystery_word.py
@@ -1,40 +1,36 @@
 import random
-import string
-available_letters = list(string.ascii_uppercase)
+# import string
+# available_letters = list(string.ascii_uppercase)
 
 
 def play_game():
     with open('words.txt') as my_file:
         read_file = my_file.read()
     word_list = read_file.split()
-    low_mystery_word = (random.choice(word_list))
+    low_mystery_word = random.choice(word_list)
     mystery_word = low_mystery_word.upper()
-    word_length = len(mystery_word)
     unpacked = [*mystery_word]
-    blank_list = unpacked[:]
 
-    i = 0
-
-    while i < len(blank_list):
-        blank_list[i] = '_'
-        i += 1
-
-    print(*blank_list)
+    blank_list = []
+    for letter in mystery_word:
+        blank_list.append('_')
 
     print(mystery_word)
 
-    print(unpacked)
-
-    print(f'The mystery word is {word_length} letters long.')
     print('')
     guess_count = 0
     guessed_list = []
     while guess_count < 8 and '_' in blank_list:
         print('')
+        print(*blank_list)
+        print('')
         print(f'Guesses remaining: {8 - guess_count}')
         print('')
-        print('Available letters to guess:')
-        print(available_letters)
+        unpack_guessed = [*guessed_list]
+        if len(guessed_list) > 0:
+            print(f'You have already guessed: {unpack_guessed}')
+        # print('Available letters to guess:')
+        # print(available_letters)
         print('')
         guess = input('Guess a letter: ').upper()
         if not guess.isalpha() or len(guess) != 1:
@@ -46,9 +42,13 @@ def play_game():
             print('You have already guessed this letter.')
             continue
         elif guess in mystery_word:
-            available_letters.remove(guess)
+            # available_letters.remove(guess)
             guessed_list.append(guess)
-            
+            for i in range(len(blank_list)):
+                # print(unpacked[i])
+                if unpacked[i] in guessed_list:
+                    blank_list[i] = unpacked[i]
+
             print('')
             print('Good guess!')
             print('')
@@ -56,7 +56,7 @@ def play_game():
             continue
         else:
             guess_count += 1
-            available_letters.remove(guess)
+            # available_letters.remove(guess)
             guessed_list.append(guess)
             print('')
             print('Incorrect guess')
@@ -64,8 +64,12 @@ def play_game():
 
     if '_' in blank_list:
         print('')
+        print(f'The mystery word was: {mystery_word}')
+        print('')
         print('Sorry! You lose!')
     else:
+        print('')
+        print(*blank_list)
         print('')
         print('Congratulations! You win!')
 
