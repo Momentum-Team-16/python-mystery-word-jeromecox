bf8cf52942b9dc44a7f0ae0be0fed398afdbb76d
jeromecox <jeromecox@hotmail.com>
2022-12-08 00:43:55 -0600

    started solution

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,38 @@
+import random
+import string
+remaining_list = list(string.ascii_uppercase)
+
+
 def play_game():
-    pass
+    with open('words.txt') as my_file:
+        read_file = my_file.read()
+    word_list = read_file.split()
+    low_mystery_word = (random.choice(word_list))
+    mystery_word = low_mystery_word.upper()
+    print(mystery_word)
+
+    print(f'The mystery word is {len(mystery_word)} letters long.')
+
+    guess_count = 0
+    guessed_list = []
+    while guess_count < 9:
+        guess = input('Guess a letter: ').upper()
+        if not guess.isalpha() or len(guess) != 1:
+            print('Invalid guess. Please guess one letter.')
+            continue
+        elif guess in guessed_list:
+            print('You have already guessed this letter.')
+            continue
+        elif guess in mystery_word:
+            guess_count += 1
+            print('Good guess!')
+            print(f'Guesses remaining: {8 - guess_count}')
+            continue
+        else:
+            guess_count += 1
+            print('Incorrect guess')
+            print(f'Guesses remaining: {8 - guess_count}')
+            continue
 
 
 if __name__ == "__main__":
