d419fe5582d1c6d85752fddad0c0417eca042c9a
jeromecox <jeromecox@hotmail.com>
2022-12-08 18:57:22 -0600

    User can choose difficulty

--- mystery_word.py
+++ mystery_word.py
@@ -1,77 +1,96 @@
 import random
 
 
+with open('words.txt') as my_file:
+    read_file = my_file.read()
+word_list = read_file.split()
+easy_list = []
+medium_list = []
+hard_list = []
+for word in word_list:
+    if len(word) in range(4, 7):
+        easy_list.append(word)
+    elif len(word) in range(6, 9):
+        medium_list.append(word)
+    elif len(word) >= 8:
+        hard_list.append(word)
+
+
+def make_word(level):
+    i = 0
+    while i == 0:
+        if level == 'easy' or level == 'e':
+            mystery_word = random.choice(easy_list).upper()
+            i += 1
+        elif level == 'medium' or level == 'm':
+            mystery_word = random.choice(medium_list).upper()
+            i += 1
+        elif level == 'hard' or level == 'h':
+            mystery_word = random.choice(hard_list).upper()
+            i += 1
+        else:
+            print('\n⛔️⚠️ PLEASE ENTER A VALID LEVEL ⚠️ ⛔️')
+            level = input('\nLevel: (e)asy / (m)edium / (h)ard: ').lower()
+    return mystery_word
+
+
 def play_game():
     play = True
     while play:
-        with open('words.txt') as my_file:
-            read_file = my_file.read()
-        word_list = read_file.split()
-        low_mystery_word = random.choice(word_list)
-        mystery_word = low_mystery_word.upper()
+        print('\n❓❔ LET\'S PLAY A GUESSING GAME!❔❓')
+        input_level = input('\nLevel: (e)asy / (m)edium / (h)ard: ').lower()
+
+        mystery_word = make_word(input_level)
+
         unpacked = [*mystery_word]
 
         blank_list = []
         for letter in mystery_word:
             blank_list.append('_')
 
-        print(mystery_word)
+        # print(f'\n{mystery_word}')
 
-        print('')
         guess_count = 0
         guessed_list = []
-        while guess_count < 1 and '_' in blank_list:
+        while guess_count < 8 and '_' in blank_list:
             print('')
+            if guess_count == 7:
+                print('\n⚠️  WARNING: ONE GUESS REMAINING ⚠️\n')
             print(*blank_list)
-            print('')
-            print(f'Guesses remaining: {8 - guess_count}')
-            print('')
-            unpack_guessed = [*guessed_list]
+            print(f'\nGuesses remaining: {8 - guess_count}')
+            unpack_guessed = ", ".join(guessed_list)
             if len(guessed_list) > 0:
-                print(f'You have already guessed: {unpack_guessed}')
-            print('')
-            guess = input('Guess a letter: ').upper()
+                print(f'\nYou have already guessed: {unpack_guessed}')
+            guess = input('\nGuess a letter: ').upper()
             if not guess.isalpha() or len(guess) != 1:
-                print('')
-                print('Invalid guess. Please guess one letter.')
+                print('\n⛔️⚠️  INVALID GUESS. Please guess one letter. ⚠️ ⛔️ ')
                 continue
             elif guess in guessed_list:
-                print('')
-                print('You have already guessed this letter.')
+                print('\nYou have already guessed this letter.')
                 continue
             elif guess in mystery_word:
                 guessed_list.append(guess)
                 for i in range(len(blank_list)):
                     if unpacked[i] in guessed_list:
                         blank_list[i] = unpacked[i]
-
-                print('')
-                print('Good guess!')
-                print('')
-
+                print('\nGood guess!')
                 continue
             else:
                 guess_count += 1
                 guessed_list.append(guess)
-                print('')
-                print('Incorrect guess')
-                continue
+                print('\nIncorrect guess')
 
         if '_' in blank_list:
-            print('')
-            print(f'The mystery word was: {mystery_word}')
-            print('')
-            print('Sorry! You lose!')
+            print(f'\nThe mystery word was: {mystery_word}')
+            print('\nSorry! You lose!')
         else:
             print('')
             print(*blank_list)
-            print('')
-            print('Congratulations! You win!')
+            print('\nCongratulations! You win!')
 
         good_input = 0
         while good_input == 0:
-            print('')
-            play_again = input('Do you want to play again? y / n: ').lower()
+            play_again = input('\nDo you want to play again? y / n: ').lower()
             if play_again != 'y' and play_again != 'n':
                 print('Invalid input')
                 continue
